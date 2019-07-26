dd3fb61476f3bb0979fa84dbda60f57ae1feb37d
Гаврись Юлия Васильевна <yv_gavris@bbgroup.local>
2019-07-26 17:39:38 +0300

    to work from home too

--- src/animate-css.js
+++ src/animate-css.js
@@ -34,39 +34,108 @@ export default (view, frames, layer) => {
         return;
       }
 
-      const keyframe = frames.find(([name]) => name === action);
+      const allKeyframes = frames.filter(([name]) => name === action);
 
-      if (!keyframe) {
+      // хочу так все переделать
+
+      const test = allKeyframes.map((keyframe) => {
+        const keyframeProps = keyframe[1] ? keyframe[1](data) : {};
+        const {easing, delay, duration} = keyframeProps;
+        if (!delay && !duration) {
+          const props = keyframe.slice(2).map(([offset,func]) => {
+            return {
+              offset,
+              ...func(data)
+            }
+          });
+          return {
+            type: 'static',
+            props
+          }
+        }
+        const props = keyframe.slice(2).reduce((acc1, [offset,func]) => {
+          return Object.entries(func(data)).reduce((acc2, [key,value]) => {
+            return {
+              ...acc2,
+              [key]: acc1[key] ? [
+                ...acc1[key],
+                value
+              ] : [value]
+            }
+          }, {});
+        }, {});
+        return {
+          type: 'animation',
+          props,
+          delay,
+          duration,
+          easing
+        }
+      });
+
+      console.warn(dom, test);
+
+      // хочу так все переделать
+
+      if (!allKeyframes.length) {
         emt({ action: `${action}-complete` });
         return;
       }
 
-      const prop = keyframe[1] ? keyframe[1](data) : {};
+      const animationInfo = allKeyframes.reduce((acc, keyframe) => {
+        const info = keyframe[1] ? keyframe[1](data) : {};
+        const duration = (info.duration || 0) * 1000;
+        const delay = (info.delay || 0) * 1000;
+        const easing = info.easing || "easeOutCubic";
 
-      const keys = keyframe.slice(2).map(([offset, prop]) => ({
-        offset,
-        ...prop(data)
-      }));
+        let keys = 0;
 
-      if (!utils.checkOffsetValidity(keys)) {
+        const prop = keyframe.slice(2).reduce((acc, [offset,prop]) => {
+          keys++
+          return Object.entries(prop(data)).reduce((acc,[key]) => key, '');
+        }, {});
+        return {
+          ...acc,
+          [prop]: {
+            duration,
+            delay,
+            easing,
+            keys
+          }
+        }
+      }, {});
+
+      const testProps = allKeyframes.map((keyframe) => keyframe[1] ? keyframe[1](data) : {});
+
+      const keys = allKeyframes.reduce((acc,keyframe) => {
+        return [
+          ...acc,
+          ...keyframe.slice(2).map(([offset,prop]) => {
+            return {
+              offset,
+              ...prop(data)
+            }
+          })
+        ]
+      }, []);
+
+      if (!keys.every((key) => utils.checkOffsetValidity(key))) {
         throw "Error: animation error, keyframe offset wrong. Valid offset: >= 0, <= 1, ascending order.";
       }
 
       const keyframes = new Map();
 
-      const { easing = "easeOutCubic" } = prop;
-      const duration = prop.duration * 1000 || 0;
-      const start = prop.start * 1000 || 0;
+      const duration = Math.max(...testProps.map(({duration}) => duration || 0)) * 1000;
+      const start = 0;
 
       const animeObj = {
         targets: dom,
-        easing,
-        duration,
+        easing: "easeOutCubic",
         complete: () => {
           emt({ action: `${action}-complete` });
           if (duration === 0) {
             dom.forEach(elem => {
-              classWatch.forEach(({ classList }) => {
+              classWatchTest.forEach(({ classList }) => {
                 classList.forEach(([className, value]) => {
                   elem.classList.toggle(className, value);
                 });
@@ -75,31 +144,30 @@ export default (view, frames, layer) => {
           }
         },
         update: anim => {
-          if (classWatch.length > 0) {
-            const { offset, classList } = classWatch[0];
-            const { progress } = anim;
-            if (progress / 100 >= offset) {
-              dom.forEach(elem => {
+          if (classWatchTest.length > 0) {
+            dom.forEach(elem => {
+              classWatchTest.forEach(({ classList }) => {
                 classList.forEach(([className, value]) => {
                   elem.classList.toggle(className, value);
                 });
               });
-              classWatch.shift();
-            }
+            });
           }
         },
         autoplay: false
       };
 
-      if (keys[0].offset === 0) {
-        const { offset, classList, ...rest } = keys.splice(0, 1)[0];
-        anime.set(dom, rest);
-      }
-
-      utils.restoreOffset(keys);
-      const classWatch = keys
-        .filter(key => key.classList)
-        .map(({ offset, classList }) => ({ offset, classList: Object.entries(classList) }));
+      // if (keys[0].offset === 0) {
+      //   const { offset, classList, ...rest } = keys.splice(0, 1)[0];
+      //   anime.set(dom, rest);
+      // }
+      //
+      // utils.restoreOffset(keys);
+      const classWatchTest = keys
+          .filter(key => {
+            return key.classList
+          })
+          .map(({ offset, classList }) => ({ offset, classList: Object.entries(classList) }));
 
       keys.forEach(key => {
         const { offset, classList, ...vars } = key;
@@ -108,11 +176,14 @@ export default (view, frames, layer) => {
             keyframes.set(key, []);
           }
           const arr = keyframes.get(key);
+
+          const info = animationInfo[key] || {delay: 0, duration: 0};
+
           arr.push({
             value,
-            duration: offset
-              ? offset * duration - arr.map(e => e.duration).reduce((p, c) => (p || 0) + (c || 0), 0)
-              : undefined
+            duration: offset === "0" ? 0 : (info.duration ? Math.round(info.duration / (info.keys - 1)) : 0),
+            delay: offset === "0" ? info.delay : 0,
+            easing: info.easing
           });
         });
       });
@@ -133,14 +204,14 @@ export default (view, frames, layer) => {
         !followers.has(e) && followers.set(e, []);
 
         followers.set(
-          e,
-          followers.get(e).filter(({ anim, params }) => {
-            if (matchesCount(params, animParams)) {
-              anim.pause();
-              return false;
-            }
-            return true;
-          })
+            e,
+            followers.get(e).filter(({ anim, params }) => {
+              if (matchesCount(params, animParams)) {
+                anim.pause();
+                return false;
+              }
+              return true;
+            })
         );
 
         followers.get(e).push({ anim: animation, params: animParams });
